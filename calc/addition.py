70602db4f15dfdbeaca10648be41ecd9da36d138
SRIRAKSHA-SARATHI <st64@njit.edu>
2021-11-04 23:53:16 -0400

    Updated calc

---
+++ calc/addition.py
@@ -0,0 +1,9 @@
+"""This is the addition calculation that is being inherits the value A and value B from the calculation class"""
+
+from calc.calculation import Calculation
+
+class Addition(Calculation):
+    """The addition class has one method to get the result of the the calculation A and B come from
+    the calculation parent class"""
+    def getResult(self):
+        return self.value_a + self.value_b
