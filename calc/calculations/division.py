8c742ba0f7c05c7a7b1774c08b2e2f52ba958fbf
SRIRAKSHA-SARATHI <st64@njit.edu>
2021-11-14 17:09:28 -0500

    Division code added

---
+++ calc/calculations/division.py
@@ -0,0 +1,8 @@
+"""This is division operation object"""
+
+from calc.history.calculations import Calculations
+class Division(Calculations):
+    """This is division class"""
+    def get_result(self):
+        """This is division class"""
+        return self.value_a / self.value_b
