75e7ff42ffc33ffe3859462176cdaea418e0ffd9
SRIRAKSHA-SARATHI <st64@njit.edu>
2021-11-16 10:33:26 -0500

    Division code fixed

--- calc/calculations/calculation.py
+++ calc/calculations/calculation.py
@@ -1,10 +1,14 @@
 """Calculation Class"""
+
+
 class Calculation:
     """ calculation abstract base class"""
+
     # pylint: disable=too-few-public-methods
-    def __init__(self,values: tuple):
+    def __init__(self, values: tuple):
         """ constructor method"""
         self.values = Calculation.convert_args_to_list_float(values)
+
     @staticmethod
     def convert_args_to_list_float(values):
         """ standardize values to list of floats"""
@@ -12,4 +16,3 @@ class Calculation:
         for item in values:
             list_values_float.append(float(item))
         return list_values_float
-    
